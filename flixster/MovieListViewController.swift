777d67c97bbd96e46b5101cd41bdc5f1e2561ddb
Colton Scott <83851807+coltscott@users.noreply.github.com>
2023-01-31 15:25:18 -0500

    my project

---
+++ flixster/MovieListViewController.swift
@@ -0,0 +1,58 @@
+//
+//  MovieListViewController.swift
+//  flixster
+//
+//  Created by Anderson David on 1/20/23.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TrackCell", for: indexPath) as! TrackCell
+        let movie = movies[indexPath.row]
+        cell.configure(with: movie)
+        return cell
+    }
+    
+    
+    var movies: [Movie] = [Movie]()
+    
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        movies = MoviesResponse.loadJson()
+        
+        for movie in movies {
+            print(movie.original_title)
+        }
+        tableView.dataSource = self
+    }
+    
+    
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let cell = sender as? UITableViewCell,
+           // Get the index path of the cell from the table view
+           let indexPath = tableView.indexPath(for: cell),
+           // Get the detail view controller
+           let detailViewController = segue.destination as? DetailViewController {
+            
+            // Use the index path to get the associated track
+            let movie = movies[indexPath.row]
+            
+            // Set the track on the detail view controller
+            detailViewController.movie = movie
+        }
+        
+        
+    }
+}
