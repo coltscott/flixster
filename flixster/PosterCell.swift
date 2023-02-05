644422f1806d0f2e3128d65f5a07e13cfcad1a1c
Colton Scott <83851807+coltscott@users.noreply.github.com>
2023-02-04 21:12:57 -0500

    project 3

---
+++ flixster/PosterCell.swift
@@ -0,0 +1,13 @@
+//
+//  PosterCell.swift
+//  flixster
+//
+//  Created by Colton Scott on 2/4/23.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
